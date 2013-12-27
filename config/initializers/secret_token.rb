cc4d75b86bf6e22d6e49bbcfcd0e71f1c2481454
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-12-26 21:00:03 -0500

    Initialize empty git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Melodymap::Application.config.secret_key_base = 'd2ec73f423115b84ac15bd877a0ff8320e72a9615bb797c049d54d7fe95f917025f8bd727c7019008413a8ec66c5aee02d0f61a64015eaced398f1b273ceef10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Melodymap::Application.config.session_store :cookie_store, key: '_melodymap_session'
