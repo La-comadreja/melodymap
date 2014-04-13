3af5e0d83ea96d57076e05c48c93bc65a48a750b
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-04-13 19:00:48 -0400

    Add the next 7 days on the calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
     @time = Time.now
+    @days = []
+    for i in 0...7
+      @day = @time.strftime("%A, %B %d")
+      @days.append(@day)
+      @time += 86400 
+    end
   end
 end
