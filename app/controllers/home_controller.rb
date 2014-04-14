3bf7b30e8dd2c9833f98802a512b8018c906f152
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-04-14 00:32:35 -0400

    submit button and need to figure out how submit works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   def index
     @time = Time.now
     @days = []
+    @searchDays = []
     for i in 0...7
       @days.append(@time.strftime("%A, %B %d"))
+      @searchDays.append(@time.strftime("%Y-%m-%d"))
       @time += 86400 
     end
   end
