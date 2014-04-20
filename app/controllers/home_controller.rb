d47144cafcff08a13ad5db61b4f46ef19373c304
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-04-20 10:02:33 -0400

    Date dropdown successfully implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   def index
     @time = Time.now
     @days = []
-    @searchDays = []
+    @search_days = []
     for i in 0...7
       @days.append(@time.strftime("%A, %B %d"))
-      @searchDays.append(@time.strftime("%Y-%m-%d"))
+      @search_days.append(@time.strftime("%Y-%m-%d"))
       @time += 86400 
     end
   end
