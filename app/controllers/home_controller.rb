37bcebfc5a94746b29d9748919a9f98007aaaa98
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-04-20 16:08:58 -0400

    made the number of days a single controller variable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
 class HomeController < ApplicationController
   def index
+    @NUM_DAYS = 7
     @time = Time.now
     @days = []
     @search_days = []
-    for i in 0...7
+    for i in 0...@NUM_DAYS
       @days.append(@time.strftime("%A, %B %d"))
       @search_days.append(@time.strftime("%Y-%m-%d"))
       @time += 86400 
     end
 
-    
+    d = params[:days]
+    d ||= 0
+    doc_string = Nokogiri::HTML(open(URI.escape("http://www.villagevoice.com/concerts/#/date:" + @search_days[d.to_i] + "/")))
   end
 end
