d1af7fdc62e69b49bef8910bbdb930df3ab3882a
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-04-26 17:51:29 -0400

    Controller is tested code from colorpicker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,37 @@ class HomeController < ApplicationController
     @search_days = []
     for i in 0...@NUM_DAYS
       @days.append(@time.strftime("%A, %B %d"))
-      @search_days.append(@time.strftime("%Y-%m-%d"))
+      @search_days.append(@time.strftime("%d+%b+%Y"))
       @time += 86400 
     end
 
-    d = params[:days]
-    d ||= 0
-    doc_string = Nokogiri::HTML(open(URI.escape("http://www.villagevoice.com/concerts/#/date:" + @search_days[d.to_i] + "/")))
+    # pages directory scrapes from http://www.mysocialist.com/concerts
+    doc_string = Nokogiri::HTML(open("pages/mysocialist.txt"))
+    @sections = doc_string.css(".listingRow")
+    @date = 0
+    if !params[:date].nil?
+      @date = params[:date].to_i
+    end
+
+    @events = []
+    @links = []
+    @addresses = []
+    @sections.each do |s|
+      t = s.to_s.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+      if t.include?("+on+" + @search_days[@date])
+        @events.append(t)
+        l = "http://www.mysocialist.com" + s.css("a")[0].to_s.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').split("\"")[1]
+        @links.append(l)
+
+        doc_string = Nokogiri::HTML(open(l))
+        s = doc_string.css(".event-venue-data")[0].to_s.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+        address = extr(s, "streetAddress") + ", " + extr(s, "addressLocality") + ", " + extr(s, "addressRegion") + " " + extr(s, "postalCode")
+        @addresses.append(address)
+      end
+    end
+  end
+
+  def extr(s, str)
+    s.split("itemprop=\"" + str + "\">")[1].split("</span>")[0]
   end
 end
